d23e1ce66800019f6cba6a4fa02e4695013c211b
Mürsel SEN <murselsen_35@hotmail.com>
2025-04-17 14:25:36 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+
+    <div className='App'>
+      <h1>Hello World</h1>
+      <h2>Welcome to React</h2>
+      <p>This is a simple React application.</p>
+      <p>It is created using Vite and React.</p>
+    </div>
+  )
+}
+
+export default App
